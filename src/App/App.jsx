a256235be1b498ce4b0bf0e31d10ce6ae8ffb802
x320608 <xian.oliveirataboada@plexus.es>
2020-09-25 10:39:20 +0200

    Wishlist like tutorial

---
+++ src/App/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './App.css';
+import WishInput from './WishInput';
+import WishList from './WishList';
+
+const initialWishes = [
+  { done: false, text: 'Travel to the moon' },
+  { done: true, text: 'Make an intro course to React' },
+  { done: true, text: 'Pay the gym' },
+  { done: false, text: 'Go to the gym' },
+];
+
+const App = () => {
+  const [wishes, setWishes] = useState(initialWishes);
+  return (
+    <div className="app">
+      <h1>My wishlist</h1>
+      <WishInput onNewWish={(wish) => setWishes([...wishes, wish])} />
+      <WishList wishes={wishes} onWishesChange={setWishes} />
+      <button
+        type="button"
+        className="wish-clear"
+        onClick={() => setWishes(wishes.filter((wish) => !wish.done))}
+      >
+        Archive done
+      </button>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/App/WishList/WishItem.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const WishItem = ({
+  done, text, id, onDoneChange,
+}) => {
+  const [age, setAge] = useState(0);
+
+  useEffect(() => {
+    let ageInterval;
+    if (done) {
+      setAge(0);
+    } else {
+      ageInterval = setInterval(() => {
+        if (done) {
+          clearInterval(ageInterval);
+        } else {
+          setAge((a) => a + 1);
+        }
+      }, 1000);
+    }
+    return () => clearInterval(ageInterval);
+  }, [done]);
+
+  return (
+    <li
+      className={classNames('wish-list__item', {
+        'wish-list__item--done': done,
+        'wish-list__item--warning': age >= 5 && age < 10,
+        'wish-list__item--danger': age >= 10,
+      })}
+    >
+      <input
+        id={id}
+        type="checkbox"
+        checked={!!done}
+        onChange={(e) => onDoneChange(e.target.checked)}
+      />
+      <label htmlFor={id}>{text}</label>
+    </li>
+  );
+};
+WishItem.propTypes = {
+  done: PropTypes.bool,
+  text: PropTypes.string,
+  id: PropTypes.string,
+  onDoneChange: PropTypes.func,
+};
+WishItem.defaultProps = {
+  done: false,
+  text: '',
+  id: '',
+  onDoneChange: () => {},
+};
+export default WishItem;
